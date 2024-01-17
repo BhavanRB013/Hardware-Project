36121a0799632a6e3b018a730b50c555e44be61c
BhavanRB013 <132537050+BhavanRB013@users.noreply.github.com>
2024-01-17 22:03:33 +0530

    Add files via upload

---
+++ Values_From_Openweather/code/Values_from_openweather/Values_from_openweather.ino
@@ -0,0 +1,104 @@
+//Libraries needed for connection
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+
+//The name and password of wifi need to be connected
+const char* ssid = "NIGGA 1";
+const char* password = "11111111";
+
+//API key from openweather
+String openWeatherMapApiKey = "815036a6ce6c133139d384f6011040b4";
+
+
+//The place need to be measured
+String city = "Chennai";
+String countryCode = "IN";
+
+//For delay 
+unsigned long lastTime = 0;
+
+unsigned long timerDelay = 10000;
+
+//to store json value from openweather
+String jsonBuffer;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
+}
+
+void loop() {
+ 
+  if ((millis() - lastTime) > timerDelay) {
+    
+    if(WiFi.status()== WL_CONNECTED){
+      String serverPath = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&APPID=" + openWeatherMapApiKey;
+      
+      //function to get the value from openweather
+      jsonBuffer = httpGETRequest(serverPath.c_str());
+      Serial.println(jsonBuffer);
+      //The json value will be stored in myobject which is a variable
+      JSONVar myObject = JSON.parse(jsonBuffer);
+  
+      
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+      Serial.print("Temperature: ");
+      Serial.println(myObject["main"]["temp"]);
+      Serial.print("Pressure: ");
+      Serial.println(myObject["main"]["pressure"]);
+      Serial.print("Humidity: ");
+      Serial.println(myObject["main"]["humidity"]);
+      Serial.print("Wind Speed: ");
+      Serial.println(myObject["wind"]["speed"]);
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  
+  http.begin(client, serverName);
+  
+ 
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  
+  http.end();
+
+  return payload;
+}
